a93a41440dfe91a25af4b2cf5a79f0bd59475b79
ks <ksumiya0318@gmail.com>
2021-09-16 02:08:10 +0900

    Global IP Checker with Gmailを追加

---
+++ global_ip_checker_mail/README.md
@@ -0,0 +1,48 @@
+# Global IP Checker with Gmail
+
+## Requirement
+
+- OS: Mac, Windows
+- python3
+  - datetime
+  - requests
+  - platform
+  - json
+  - import pickle
+  - import os.path
+  - from googleapiclient.discovery import build
+  - from google_auth_oauthlib.flow import InstalledAppFlow
+  - from google.auth.transport.requests import Request
+  - import base64
+  - from email.mime.text import MIMEText
+  - from apiclient import errors
+
+
+
+## Tested Environment
+
+- Mac
+  - macOS Big Sur 11.5.2 on M1 MacBook Air 2020
+- Windows
+  - Windows 10 pro on Desktop PC 
+
+
+
+## Install Gmail Libraries
+
+```
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+```
+
+
+
+## Usage
+
+```shell
+python3 main.py
+or
+python main.py
+```
+
+
+
---
+++ global_ip_checker_mail/main.py
@@ -0,0 +1,70 @@
+import sys
+import datetime
+import requests
+import platform
+import json
+import gmailSender
+
+def getGlobalIP():
+    response = requests.get('https://ifconfig.io/ip')
+    _globalIP = ''.join(response.text.splitlines())
+    return _globalIP
+
+def readPreviousGlobalIP(_fileName):
+    try:
+        f = open(_fileName, 'r', encoding='UTF-8')
+        _previousGlobalIP = f.read()
+        f.close()
+    except:
+        _previousGlobalIP = "123.123.123.123"
+    return _previousGlobalIP
+
+def updateMailSettings(_jsonPath, _message):
+    _json = json.loads(open(_jsonPath, 'r').read())
+    _json["message"] = _message
+    with open(_jsonPath, 'w') as f:
+        json.dump(_json, f, indent=2, ensure_ascii=False)
+
+def checkGlobalIPChangingAndNotification(_globalIP, _previousGlobalIP):
+    if _globalIP != _previousGlobalIP:
+        _platformSystem = platform.system()
+        _message = "New Global IP: " + _globalIP
+        updateMailSettings("mail-settings.json", _message)
+        gmailSender.mainProcess()
+
+        _changedFlag = ", changed"
+    else:
+        _changedFlag = ""
+    return _changedFlag
+
+def writeGlobalIPIntoLog(_logFileName, _globalIP, _changedFlagStr):
+    dtNow = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    f = open(_logFileName, 'a', encoding='UTF-8')
+    f.write(dtNow + ', ' + _globalIP + _changedFlagStr + '\n')
+    f.close()
+
+def writeGlobalIPIntoTxt(_checkFileName, _globalIP):
+    f = open(_checkFileName, 'w', encoding='UTF-8')
+    f.write(_globalIP)
+    f.close()
+
+def mainProcess():
+    checkFileName = 'global-ip.txt'
+    logFileName = 'global-ip-log.txt'
+    globalIP = getGlobalIP()
+    if IsPrint: print(globalIP)
+    previousGlobalIP = readPreviousGlobalIP(checkFileName)
+    changedFlagStr = checkGlobalIPChangingAndNotification(globalIP, previousGlobalIP)
+    writeGlobalIPIntoLog(logFileName, globalIP, changedFlagStr)
+    writeGlobalIPIntoTxt(checkFileName, globalIP)
+
+
+# ==============================================================================================================
+if __name__ == "__main__":
+    IsPrint = True
+    if len(sys.argv) == 2: 
+        if sys.argv[1] == "noPrint": 
+            IsPrint = False
+
+    mainProcess()
+
